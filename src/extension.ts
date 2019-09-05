2aa5600ba2ab3c4d1daefede2ddaeca52af18af8
mziyabo <mziyabo@live.com>
2019-09-05 23:44:53 +0200

    Added sentiment decorations

--- src/extension.ts
+++ src/extension.ts
@@ -6,16 +6,86 @@ let AWS = require("aws-sdk");
 export function activate(context: vscode.ExtensionContext) {
     let disposable = vscode.commands.registerCommand('extension.analyze', () => {
 
-        vscode.window.showInformationMessage("vscode-sentiment loaded");
-        //TODO: get text and analyze
+        console.log("vscode-sentiment loaded");
+        //TODO: clear previous decorations
+        //Get text and analyze
         AnalyzeWithSentiment();
     });
+
+    let activeEditor = vscode.window.activeTextEditor;
+    vscode.workspace.onDidChangeTextDocument(event => {
+        if (activeEditor && event.document === activeEditor.document) {
+            AnalyzeWithSentiment();
+        }
+    }, null, context.subscriptions);
 
     context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {
+}
+
+function AnalyzeWithSentiment() {
+
+    // Perform Sentiment Analysis
+    let sentiment = new Sentiment({});
+    let options = {
+        language: "en",
+        extras: null
+    };
+
+    let line = 0;
+    let score = 0;
+    if (vscode.window.activeTextEditor != null) {
+        let text = vscode.window.activeTextEditor.document.getText();
+        let sentences = text.split("\n");
+
+        sentences.forEach(sentence => {
+            // TODO: analyze each sentence here
+            let result = sentiment.analyze(sentence);
+            score += result.score;    // Score: -2, Comparative: -0.666
+
+            var selection = vscode.window.activeTextEditor.document.lineAt(line).range;
+            Decorate(score, selection);
+
+            line += 1;
+        });
+
+        vscode.window.setStatusBarMessage(`Sentiment: ${score}`);
+    }
+}
+
+function Decorate(score, selection) {
+
+    // TODO: Create sentiment/s
+    const negativeSentimentDecorator = vscode.window.createTextEditorDecorationType({
+
+        backgroundColor: { id: 'codesentiment.NegativeSentiment' }
+
+    });
+
+    const positiveSentimentDecorator = vscode.window.createTextEditorDecorationType({
+        backgroundColor: { id: 'codesentiment.PositiveSentiment' }
+    });
+
+    let selections = [selection];
+    if (score < 0) {
+        // Negative sentence
+        // Extremely negative
+        //if (score < -3) {
+        // Decorate
+        vscode.window.activeTextEditor.setDecorations(negativeSentimentDecorator, selections);
+        //}
+
+    }
+    else if (score > 1) {
+        // Positive sentence
+        // Very Positive
+        // if (score > 3) {
+        vscode.window.activeTextEditor.setDecorations(positiveSentimentDecorator, selections);
+        // }
+    }
 }
 
 function AnalyzeWithComprehend() {
@@ -34,28 +104,4 @@ function AnalyzeWithComprehend() {
         if (err) console.log(err, err.stack); // an error occurred
         else console.log(data);           // successful response
     });
-}
-
-function AnalyzeWithSentiment() {
-
-    let sentiment = new Sentiment({});
-    let options = {
-        language: "en",
-        extras: null
-    };
-
-    let score = 0;
-    if (vscode.window.activeTextEditor != null) {
-        let text = vscode.window.activeTextEditor.document.getText();
-        let sentences = text.split("\n");
-
-        sentences.forEach(sentence => {
-            // TODO: analyze each sentence here
-            let result = sentiment.analyze(sentence);
-
-            score += result.score;    // Score: -2, Comparative: -0.666
-        });
-
-        vscode.window.setStatusBarMessage(`Sentiment: ${score}`);
-    }
 }
